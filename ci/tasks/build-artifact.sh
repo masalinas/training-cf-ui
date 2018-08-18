488bd77e0c9971b0f116ce7f334d8376e89f6090
Miguel Salinas Gancedo <miguel@thingtrack.com>
2018-08-18 11:53:44 +0200

    update concourse scripts

--- ci/tasks/build-artifact.sh
+++ ci/tasks/build-artifact.sh
@@ -1,8 +1,16 @@
 #!/bin/sh
 
+# install last angular cli
+npm install -g @angular/cli
+
+# set into source foulder
 cd training-cf-ui
 
+# check angular cli version
 ng -v
 
+# install dependencies
 #npm install
+
+# compile in production mode
 #ng build --prod
