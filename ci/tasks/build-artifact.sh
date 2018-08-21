f3e1a1b37aca8208f42db17cea36dd143843818d
Miguel Salinas Gancedo <miguel@thingtrack.com>
2018-08-21 17:36:54 +0200

    last update

--- ci/tasks/build-artifact.sh
+++ ci/tasks/build-artifact.sh
@@ -1,14 +1,14 @@
 #!/bin/sh
 
-echo 'Check all folders '
-ls /tmp -all
+#echo 'Check all folders'
+#ls /tmp -all
 
-echo 'Before cd'
-pwd
-ls -all
+#echo 'Before cd'
+#pwd
+#ls -all
 
-echo 'List build folder'
-ls /tmp/build -all
+#echo 'List build folder'
+#ls /tmp/build -all
 
 # set into source foulder
 cd training-cf-ui
@@ -22,9 +22,9 @@ npm install
 #ll ./node_modules/.cache
 
 # debug tasks
-ls -all
-node -v
-npm -v
+#ls -all
+#node -v
+#npm -v
 
 # check angular cli version
 echo 'installing angular CLI dependency'
@@ -37,5 +37,15 @@ ng -v
 ng build --prod
 
 # debug tasks
-pwd
-ls -all
+#pwd
+#ls -all
+
+echo 'preparing dist folder'
+cd ..
+mkdir training-cf-ui-dist
+cp ./training-cf-ui/manifest-cloud.yml ./training-cf-ui-dist
+cp ./training-cf-ui/package.json ./training-cf-ui-dist
+cp ./training-cf-ui/server.js ./training-cf-ui-dist
+cp -R ./training-cf-ui/dist ./training-cf-ui-dist
+
+ls ./training-cf-ui-dist -all
