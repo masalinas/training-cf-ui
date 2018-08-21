38262b33b58004d5f0557417f38d7a77e2dc5c2f
Miguel Salinas Gancedo <miguel@thingtrack.com>
2018-08-21 14:32:59 +0200

    last update

--- ci/tasks/build-artifact.sh
+++ ci/tasks/build-artifact.sh
@@ -7,8 +7,8 @@ cd training-cf-ui
 npm install
 
 # remove .cache node_modules to avoid mode errors
-rm ./node_modules/.cache/ -R
-ll ./node_modules/.cache
+#rm ./node_modules/.cache/ -R
+#ll ./node_modules/.cache
 
 # debug tasks
 ls -all
@@ -19,10 +19,11 @@ npm -v
 npm install -g @angular/cli
 
 # debug tasks
-#ng -v
+ng -v
 
-# compile in production mode
+# compile in production mode (AOT mode)
 ng build --prod
 
 # debug tasks
+pwd
 ls -all
