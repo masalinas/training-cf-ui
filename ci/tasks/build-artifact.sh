ee51d8b51ef3ba2ecbdf723ab2afbc753f6c0156
Miguel Salinas Gancedo <miguel@thingtrack.com>
2018-08-22 09:32:22 +0200

    last update

--- ci/tasks/build-artifact.sh
+++ ci/tasks/build-artifact.sh
@@ -1,52 +1,32 @@
 #!/bin/sh
 
-#echo 'Check all folders'
-#ls /tmp -all
-
-#echo 'Before cd'
-#pwd
-#ls -all
-
-#echo 'List build folder'
-#ls /tmp/build -all
-
-# set into source foulder
-echo 'set into App folder'
+# set into App folder
+echo '1. set into App folder'
 cd training-cf-ui
 
-# install dependencies
-echo 'pulling App dependencies'
+# pulling App dependencie
+echo '2. pulling App dependencies'
 npm install
 
 # remove .cache node_modules to avoid mode errors
 #rm ./node_modules/.cache/ -R
-#ll ./node_modules/.cache
 
-# debug tasks
-#ls -all
-#node -v
-#npm -v
-
-# check angular cli version
-echo 'installing angular CLI dependency'
+# installing angular CLI dependency
+echo '3. installing angular CLI dependency'
 npm install -g @angular/cli
 
-# debug tasks
+# checking andular dependencies
+echo '4. checking andular dependencies'
 ng -v
 
-# compile in production mode (AOT mode)
+# compile App in production mode (AOT mode)
+echo '5. compile App in production mode (AOT mode)'
 ng build --prod
 
-# debug tasks
-#pwd
-#ls -all
-
-# preparing build folder
-echo 'preparing App dist folder'
+# preparing App dist folder
+echo '6. preparing App dist folder'
 
 cp package.json ../training-cf-ui-dist
 cp server.js ../training-cf-ui-dist
 cp -R ./dist ../training-cf-ui-dist
 cp manifest-cloud.yml ../training-cf-ui-dist
-
-#ls ./build -all
