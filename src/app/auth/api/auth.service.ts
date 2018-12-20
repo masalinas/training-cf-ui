2744a086251addd172a6b5d5018dad9e3bdcbe54
Miguel Salinas Gancedo <miguel@thingtrack.com>
2018-12-20 13:27:32 +0100

    last update

--- src/app/auth/api/auth.service.ts
+++ src/app/auth/api/auth.service.ts
@@ -1,14 +1,13 @@
-import { Inject, Injectable, Optional } from '@angular/core';
+import { Injectable } from '@angular/core';
 
-import { Observable }                                        from 'rxjs';
+import { Observable } from 'rxjs';
 
-import { LoginForm, AuthControllerService } from '../../shared/sdk'
+import { AuthControllerService } from '../../shared/sdk'
 import { Configuration } from '../../shared/sdk/configuration';
 
 @Injectable()
 export class AuthService {
     private configuration: Configuration;
-    private loginForm : LoginForm;
 
     constructor(private authControllerService: AuthControllerService) { }
 
@@ -16,11 +15,7 @@ export class AuthService {
         return this.configuration;
     }
 
-    public login(username: string, password: string): Observable<any> {        
-        this.loginForm = {};
-        this.loginForm.username = username;
-        this.loginForm.password = password;
-
-        return this.authControllerService.authenticateUserUsingPOST(this.loginForm);
+    public login(username: string, password: string): Observable<any> {
+        return this.authControllerService.authenticateUserUsingPOST({username: username, password: password});
     }        
 }
