dd66d953ac2ee2a36a7fd3a51d7a4527e09d65ec
Miguel Salinas Gancedo <miguel@thingtrack.com>
2018-12-20 20:51:01 +0100

    last update

--- src/app/auth/api/auth.service.ts
+++ src/app/auth/api/auth.service.ts
@@ -3,34 +3,24 @@ import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 
 import { AuthControllerService, ConfigurationParameters } from '../../shared/sdk'
-import { Configuration } from '../../shared/sdk/configuration';
 
 /** import angular environment variables **/
 import { environment } from '../../../environments/environment';
 
 @Injectable()
 export class AuthService {
-    private configuration: Configuration;
-
     constructor(private authControllerService: AuthControllerService) { }
 
-    public getConfiguration() : Configuration {
-        return this.configuration;
-    }
-
     public setConfiguration(credentials) {
-        let apiKeys:  {[ key: string ]: string} = {};
-        credentials["Authorization"] = credentials.Authorization;    
-    
-        const params: ConfigurationParameters = {
+          const params: ConfigurationParameters = {
             basePath: environment.basePath,
             username: credentials.username,
-            apiKeys: apiKeys,
             accessToken: credentials.accessToken,
             withCredentials: false
         }
 
-        return this.configuration = new Configuration(params);
+        // save in localstorage my configuration
+        localStorage.setItem('params', JSON.stringify(params));
     }
 
     public login(username: string, password: string): Observable<any> {
--- src/app/auth/auth.module.ts
+++ src/app/auth/auth.module.ts
@@ -9,10 +9,33 @@ import { environment } from '../../environments/environment';
 import { ApiModule, Configuration, ConfigurationParameters } from '../shared/sdk';
 
 export function apiConfigFactory (): Configuration {
+    // recover params credentials if exist
+    let params = localStorage.getItem('params');
+
+    if (params == null)
+        return null;
+
+    let result = JSON.parse(params);
+
+    // create Configuration Parameters
     let credentials:  {[ key: string ]: string} = {};
+    credentials["Authorization"] = 'Bearer ' + result.accessToken;    
+
+    const configurationParameters: ConfigurationParameters = {
+        basePath: result.basePath,
+        username: result.username,
+        apiKeys: credentials,
+        accessToken: 'Bearer ' + result.accessToken,
+        withCredentials: false
+      }
+    
+    // return configuration
+    return new Configuration(configurationParameters);
+
+    /*let credentials:  {[ key: string ]: string} = {};
     credentials["Authorization"] = 'Bearer ' + 'eyJhbGciOiJIUzUxMiJ9.eyJzdWIiOiJhZG1pbiIsImlhdCI6MTU0NTI5NzkwMSwiZXhwIjoxNTQ1Mzg0MzAxfQ.gmzp7yQA7UXpYdBz8k9lVI_Uv02sHHdKgxGZqdJDJIGNkyXW7GkfxOmAMdLpZl7KtfXXebyQP63A4ggyyXzesw';    
     
-    const params: ConfigurationParameters = {
+    const configurationParameters: ConfigurationParameters = {
       basePath: environment.basePath,
       username: 'admin',
       password: 'thingtrack',
@@ -21,7 +44,7 @@ export function apiConfigFactory (): Configuration {
       withCredentials: false
     }
   
-    return new Configuration(params);
+    return new Configuration(configurationParameters);*/
 }
 
 @NgModule({
